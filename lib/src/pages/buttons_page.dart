49c9935401c98912b0c2ee75ab8e13c723f0d498
Antonio Aguila <aguilantonio16@gmail.com>
2020-03-24 01:11:12 -0600

    backdrop filter added

--- lib/src/pages/buttons_page.dart
+++ lib/src/pages/buttons_page.dart
@@ -144,25 +144,28 @@ class ButtonsPage extends StatelessWidget {
 
   Widget _createRoundedButton( Color color, IconData icon, String text) {
     return ClipRRect(
-      child: Container(
-        height: 180.0,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(62, 66, 107, 0.7),
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            SizedBox(height: 5.0,),
-            CircleAvatar(
-              backgroundColor: color,
-              radius: 35.0,
-              child: Icon(icon, color: Colors.white, size: 30.0),
-            ),
-            Text(text, style: TextStyle(color: Colors.pinkAccent)),
-            SizedBox(height: 5.0,)
-          ],
+      child: BackdropFilter(
+        filter: ImageFilter.blur( sigmaX: 10.0, sigmaY: 10.0 ),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 0.7),
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0,),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icon, color: Colors.white, size: 30.0),
+              ),
+              Text(text, style: TextStyle(color: Colors.pinkAccent)),
+              SizedBox(height: 5.0,)
+            ],
+          ),
         ),
       ),
     );
